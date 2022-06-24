87698a5b96a94282cd5c5b5a49f10edadd40ab09
tomcat118 <80212154+tomcat118@users.noreply.github.com>
2022-06-23 23:00:36 -0400

    USER SIGNUP&LOGIN PAGE

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,25 @@
 // Import the functions you need from the SDKs you need
+import {useState} from 'react';
 import {initializeApp} from 'firebase/app';
 import { getFirestore } from 'firebase/firestore/lite';
-import { getAuth } from 'firebase/auth'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword,getAuth, onAuthStateChanged, signOut } from 'firebase/auth'
+import { useDebugValue, useEffect } from 'react';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyBgXbpNkQt7jJ1-V8_j9y4suxO3SZ3Udks',
-  authDomain: 'react-firebase-email-log-cbf7b.firebaseapp.com',
-  projectId: 'react-firebase-email-log-cbf7b',
-  storageBucket: 'react-firebase-email-log-cbf7b.appspot.com',
-  messagingSenderId: '83227227855',
-  appId: '1:83227227855:web:62d718d11078e59c79a9ba',
+  apiKey: "AIzaSyAeTn7HVXjKq3Zad1JQgIpossZfj1PSxl0",
+  authDomain: "online-phar.firebaseapp.com",
+  projectId: "online-phar",
+  storageBucket: "online-phar.appspot.com",
+  messagingSenderId: "589597086290",
+  appId: "1:589597086290:web:9597b836fbf68fcba1458b"
 };
+
+// hook to update the real time states
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -22,4 +28,23 @@ const auth = getAuth(app)
 
 export {app};
 export {auth};
+export function signup(email,password) {
+  return createUserWithEmailAndPassword(auth, email,password);
+}
+export function useAuth(){
+  const [currentUser, setcurrentUser] = useState();
+  useEffect (() =>{
+  const usesub = onAuthStateChanged(auth, user => setcurrentUser(user));
+  return usesub;
+  }, [])
+
+  return currentUser;
+}
+export function logOut(){
+  return signOut(auth);
+}
+
+export function login(email, password){
+  return signInWithEmailAndPassword(auth, email, password);
+}
 export default db;
